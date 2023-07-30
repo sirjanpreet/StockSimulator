e3ad0450ea20e1225ae92e66652b899366b13795
sirjanpreet <56618410+sirjanpreet@users.noreply.github.com>
2023-07-30 05:54:11 +0000

    Sat, Jul 29, 2023, 10:54 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -127,7 +127,7 @@ def register():
         db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, generate_password_hash(password))
         return render_template("register.html")
 
-    return render_template("apology")
+    return render_template("apology.html")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
