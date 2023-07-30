ec55c2b777c5505a14d20b20cd9c8dac4eee68ad
sirjanpreet <56618410+sirjanpreet@users.noreply.github.com>
2023-07-30 09:18:31 +0000

    Sun, Jul 30, 2023, 2:18 AM -07:00

--- finance/app.py
+++ finance/app.py
@@ -163,7 +163,8 @@ def register():
     if not(username or password or confirmation or confirmation == password or not has_repeat):
         return apology("Sorry, invalid username or passwords do not match")
 
-    db.execute("INSERT INTO users (id, username, hash) VALUES(?, ?)", username, generate_password_hash(password))
+    db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, generate_password_hash(password))
+    session["user_id"] = db.execute("SELECT id FROM users WHERE username = ?", username)
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
