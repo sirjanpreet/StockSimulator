4b15c8a78d8ee6801515a52c855e2cf0e7cfd40c
sirjanpreet <56618410+sirjanpreet@users.noreply.github.com>
2023-08-01 20:52:37 +0000

    Tue, Aug 1, 2023, 1:52 PM -07:00

--- finance/app.py
+++ finance/app.py
@@ -88,7 +88,7 @@ def buy():
         db.execute("UPDATE users SET cash = ? WHERE id = ?", cash_available - price_total, session["user_id"])
 
         #check if user has already has that stock, if so add shares to that stock, else insert a new stock
-        stocks = db.execute("SELECT stock_symbol FROM stocks WHERE stock_symbol = ? AND id = ?", symbol, session["user_id"])
+        stocks = db.execute("SELECT stock_symbol FROM stocks WHERE stock_symbol = ? AND user_id = ?", symbol, session["user_id"])
         if len(stocks) == 0:
             db.execute("INSERT INTO stocks (user_id, stock_symbol, shares) VALUES (?, ?, ?)", session["user_id"], symbol, shares)
             print("tihs")
