e92ce252002b35a2c8e60e6127c40962e27b9073
sirjanpreet <56618410+sirjanpreet@users.noreply.github.com>
2023-08-02 16:55:25 +0000

    Wed, Aug 2, 2023, 9:55 AM -07:00

--- finance/app.py
+++ finance/app.py
@@ -41,7 +41,7 @@ def index():
     cash_available = users[0]["cash"]
     grand_total = cash_available
     for stock in stocks:
-        current_price = lookup(stock["stock_symbol"])["price"]
+        current_price = lookup(stock["stock_symbol"])["price"] #forgot to write ["price]
         stock["current_price"] = current_price
         total_holding = current_price * stock["shares"]
         stock["total_holding"] = total_holding
